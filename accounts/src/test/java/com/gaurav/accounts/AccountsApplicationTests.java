40f6237b7676fb576853ad4072a0921195828a68
Gaurav Salunke <gauravsalunke15@gmail.com>
2024-03-01 10:17:12 +0530

    Initial accounts project setup

---
+++ accounts/src/test/java/com/gaurav/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaurav.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
