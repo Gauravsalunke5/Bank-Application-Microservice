ddbc2ae40b81c40c976d8ef73727e10c482d2a94
Gaurav Salunke <gauravsalunke15@gmail.com>
2024-03-09 17:22:25 +0530

    Created Dto

---
+++ accounts/src/main/java/com/gaurav/accounts/dto/CustomerDto.java
@@ -0,0 +1,12 @@
+package com.gaurav.accounts.dto;
+
+import jakarta.persistence.Column;
+
+public class CustomerDto {
+
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+}
